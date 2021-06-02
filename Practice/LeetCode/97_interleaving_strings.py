1da3debe8459722b55f43d035b5bcbc4506e6b70
Paryaxify <psingh4_be19@thapar.edu>
2021-06-02 20:08:15 +0530

    [M] BFS implementation for interleaving strings

--- Practice/LeetCode/97_interleaving_strings.py
+++ Practice/LeetCode/97_interleaving_strings.py
@@ -7,20 +7,29 @@ def isInterleaving(s1: str, s2: str, s3: str) -> bool:
     if l1 + l2 != l3:
         return False
 
+    # set starting pointer to 0 for the strings
     queue = [(0, 0)]
+    # add to visited set if the index has been visited
     visited = set((0, 0))
 
     while queue:
+        # starting from front of queue
         s1_index, s2_index = queue.pop(0)
         if s1_index + s2_index == l3:
             return True
+
+        # check whether current index is less than the length of list
+        # check if current index is the current index of the interleaved string
+        # check whether the current queue exists in visited
         if s1_index + 1 <= l1 and s1[s1_index] == s3[s1_index + s2_index] and (s1_index + 1, s2_index) not in visited:
+            # add the index to queue and mark it as visited
             queue.append((s1_index + 1, s2_index))
             visited.add((s1_index + 1, s2_index))
         if s2_index + 1 <= l2 and s2[s2_index] == s3[s1_index + s2_index] and (s1_index, s2_index + 1) not in visited:
             queue.append((s1_index, s2_index + 1))
             visited.add((s1_index, s2_index + 1))
 
+        # loop continues until the queue isnt empty
     return False
 
 
